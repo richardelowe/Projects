e5328223783d0f8b96246fa8f4ce58f1e374e740
Richard Lowe <rlowe@tibco.com>
2020-02-11 06:04:28 +0800

    first commit

---
+++ ScriptTest/ScriptTest/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/ScriptTest/Tests/DBQuerySuccess.bwt#/"/>
+  <EmulationDataFile>ACED000574001244425175657279537563636573732E627774</EmulationDataFile>
+</emulation:BWTFile>
